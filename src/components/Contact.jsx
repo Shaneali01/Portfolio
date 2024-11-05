3b725e3232e06a606bf76d3b330e345ea54cfa23
SHAN-E-ALI SHAH <shanealisa456@gmail.com>
2024-11-05 14:47:56 +0500

    contact color change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,18 +37,18 @@ function Contact() {
     <>
       <div
         name="Contact"
-        className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-16"
+        className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-16 "
       >
         <h1 className="text-3xl font-bold mb-4">Contact me</h1>
         <span>Please fill out the form below to contact me</span>
         <div className="flex flex-col items-center justify-center mt-5">
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className="bg-slate-200 w-96 px-8 py-6 rounded-xl"
+            className="bg-slate-200 dark:text-slate-200 dark:bg-slate-800 w-96 px-8 py-6 rounded-xl"
           >
             <h1 className="text-xl font-semibold mb-4">Send Your Message</h1>
             <div className="flex flex-col mb-4">
-              <label className="block text-gray-700">FullName</label>
+              <label className="block dark:text-slate-200 text-gray-700">FullName</label>
               <input
                 {...register("name", { required: true })}
                 className="shadow rounded-lg appearance-none border py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -60,7 +60,7 @@ function Contact() {
               {errors.name && <span>This field is required</span>}
             </div>
             <div className="flex flex-col mb-4">
-              <label className="block text-gray-700">Email Address</label>
+              <label className="block dark:text-slate-200 text-gray-700">Email Address</label>
               <input
                 {...register("email", { required: true })}
                 className="shadow rounded-lg appearance-none border py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -72,7 +72,7 @@ function Contact() {
               {errors.email && <span>This field is required</span>}
             </div>
             <div className="flex flex-col mb-4">
-              <label className="block text-gray-700">Message</label>
+              <label className="block dark:text-slate-200 text-gray-700">Message</label>
               <textarea
                 {...register("message", { required: true })}
                 className="shadow rounded-lg appearance-none border py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
